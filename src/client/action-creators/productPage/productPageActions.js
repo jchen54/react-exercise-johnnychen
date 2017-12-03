cd76547b8b2030afefdbee8b3fbc61059b67517c
Johnny Chen <johnny.chen54@gmail.com>
2017-12-02 17:08:10 -0800

    Optimize solution with object mapping

--- src/client/action-creators/productPage/productPageActions.js
+++ src/client/action-creators/productPage/productPageActions.js
@@ -1,13 +1,13 @@
 import actionTypes from './productPageActionTypes';
 
 export function setProductColor(productColor) {
-  let imageSrc = 'https://dl.dropboxusercontent.com/s/38pwoyygf2art8v/var1.png';
-  if (productColor === 'Lavender') {
-    imageSrc = 'https://dl.dropboxusercontent.com/s/w1ihqb5hadv228g/var2.png';
-  }
+  const colorsMap = {
+    Verdant: 'https://dl.dropboxusercontent.com/s/38pwoyygf2art8v/var1.png',
+    Lavender: 'https://dl.dropboxusercontent.com/s/w1ihqb5hadv228g/var2.png',
+  };
   return {
     type: actionTypes.SET_PRODUCT_COLOR,
     productColor,
-    imageSrc,
+    imageSrc: colorsMap[productColor],
   };
 }
